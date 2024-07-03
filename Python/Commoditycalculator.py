46475eecfe6f9754a47dbdacfd9bcd08715a6582
VishMukk2024 <mukkapativishal321@outlook.com>
2024-07-03 18:13:19 +0530

    Add commodity file

---
+++ Python/Commoditycalculator.py
@@ -0,0 +1,6 @@
+overallsum=int(input("Enter the Overall Amount:"))
+distributionamount=overallsum/2
+sindhu=distributionamount/2
+teja=sindhu/2
+electricity=teja
+print(f"Amount Entered:{overallsum}\ndistributedamount:{distributionamount}\nSindhu Amount:{sindhu}\nTeja Amount:{teja}\nElecticity:{electricity}")
