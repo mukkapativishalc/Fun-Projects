0c52fc87881eb558d69370a494eee5750ea9b5ef
VishMukk2024 <mukkapativishal321@outlook.com>
2024-07-14 23:58:41 +0530

    Updated Payments

--- Python/Commoditycalculator.py
+++ Python/Commoditycalculator.py
@@ -4,4 +4,4 @@ sindhu=distributionamount*0.45
 teja=(distributionamount*0.55)/3
 electricity=teja
 Mom=teja
-print(f"Amount Entered:{overallsum:.2f}\ndistributedamount:{distributionamount:.2f}\nSindhu Amount:{sindhu:.2f}\nTeja Amount:{teja:.2f}\nElecticity:{electricity:.2f}\nMom Amount:{Mom:.2f}")
+print(f"Amount Entered:{overallsum:.2f}\ndistributedamount:{distributionamount:.2f}\n Sindhu Amount:{sindhu:.2f}\nTeja Amount:{teja:.2f}\nElecticity:{electricity:.2f}\nMom Amount:{Mom:.2f}")
